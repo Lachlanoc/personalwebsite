f62c398a7ce91f9365f2e44ab9710d484dfabc42
Lachlan <lachlanoco@gmail.com>
2025-07-17 20:41:13 +1000

    Updated favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        {/* <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/favicon/apple-touch-icon.png"
@@ -52,7 +57,7 @@ export default function RootLayout({
         <meta
           name="msapplication-config"
           content="/favicon/browserconfig.xml"
-        />
+        /> */}
         <meta name="theme-color" content="#000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
