cbc07a0b109bd10e39be522607abd7e432ac27f8
Lachlan <lachlanoco@gmail.com>
2025-12-09 23:54:14 +1000

    fix: Breakpoints

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -15,31 +15,30 @@ const Header = () => {
   };
 
   return (
-    <section className="flex-col md:flex-row flex mb-2 md:mb-0 items-center md:justify-between ">
-      <h1 className="flex flex-col sm:flex-row text-3xl md:text-7xl text-center sm:text-left font-bold tracking-tight leading-tight 
-      mt-6 mb-2 md:mb-12 md:pr-8">
-        <Link href="/" onClick={handleHomeClick} className="md:mr-2">
+    <section className="flex flex-col xl:flex-row mb-2 xl:mb-0 items-center xl:justify-between">
+      <h1 className="flex flex-col sm:flex-row text-3xl xl:text-7xl text-center sm:text-left font-bold tracking-tight leading-tight mt-6 mb-2 xl:mb-12 xl:pr-8">
+        <Link href="/" onClick={handleHomeClick} className="xl:mr-2">
           Lachlan&nbsp;
         </Link>
         <Link href="/" onClick={handleHomeClick}>
           O'Connell
         </Link>
       </h1>
-      <div className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <div className="text-center xl:text-left text-lg mt-5 xl:pl-8">
         <Link 
           href="/"
           onClick={handleHomeClick}
-          className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight hover:underline mr-10">
+          className="text-2xl xl:text-4xl font-bold tracking-tight xl:tracking-tighter leading-tight hover:underline mr-8 xl:mr-10">
           Home
         </Link>
         <Link 
           href="/about"
-          className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight hover:underline mr-10">
+          className="text-2xl xl:text-4xl font-bold tracking-tight xl:tracking-tighter leading-tight hover:underline mr-8 xl:mr-10">
           About
         </Link>
         <Link 
           href="/contact"
-          className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight hover:underline">
+          className="text-2xl xl:text-4xl font-bold tracking-tight xl:tracking-tighter leading-tight hover:underline">
           Contact
         </Link>
       </div>
