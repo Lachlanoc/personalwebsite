72bddea1b8915fccc9c151108c3bdcec4e556032
Lachlan <lachlanoco@gmail.com>
2026-02-03 00:22:49 +1000

    fix: darkmode quote text colour

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -22,7 +22,8 @@ export default async function Post(props: Params) {
             prose-headings:mt-8 prose-headings:font-semibold
             prose-h1:text-3xl sm:prose-h1:text-5xl prose-h1:font-extrabold sm:prose-h1:font-bold prose-h2:text-3xl
             prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-2xl prose-h6:text-xl overflow-x-auto
-            dark:text-slate-400 dark:prose-headings:text-slate-400 dark:prose-a:text-slate-400 overflow-hidden max-w-none mb-16">
+            dark:text-slate-400 dark:prose-headings:text-slate-400 dark:prose-a:text-slate-400 dark:prose-blockquote:text-slate-400
+            overflow-hidden max-w-none mb-16">
           <PostHeader
             title={post.title}
             coverImage={post.coverImage}
