b39206c4eeb5f444b2fbb3139b89a441ac493602
Lachlan <lachlanoco@gmail.com>
2026-02-17 23:47:49 +1000

    fix: code block styling

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -22,7 +22,8 @@ export default async function Post(props: Params) {
             prose-headings:mt-8 prose-headings:font-semibold
             prose-h1:text-3xl sm:prose-h1:text-5xl prose-h1:font-extrabold sm:prose-h1:font-bold prose-h2:text-3xl
             prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-2xl prose-h6:text-xl overflow-x-auto
-            dark:text-slate-400 dark:prose-headings:text-slate-400 dark:prose-a:text-slate-400 dark:prose-blockquote:text-slate-400 dark:prose-strong:text-slate-300
+            dark:text-slate-400 dark:prose-headings:text-slate-400 dark:prose-a:text-slate-400 dark:prose-blockquote:text-slate-400 dark:prose-strong:text-slate-300 dark:prose-code:text-gray-200
+            prose-code:before:content-none prose-code:after:content-none prose-code:bg-black/5 prose-code:rounded prose-code:px-1.5 prose-code:py-0.5 prose-code:font-normal dark:prose-code:bg-white/10
             overflow-hidden max-w-none mb-16">
           <PostHeader
             title={post.title}
